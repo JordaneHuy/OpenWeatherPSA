bb6831932b9d7ff2340633a9ae5046a9ad3389b8
HUY Jordane (External) <jordane.huy-ext@kering.com>
2023-02-28 15:25:06 +0100

    init

---
+++ OpenWeather/Location.swift
@@ -0,0 +1,16 @@
+//
+//  Location.swift
+//  OpenWeather
+//
+//  Created by Jordane HUY on 28/02/2023.
+//
+
+import Foundation
+
+struct Location {
+    var name: String
+    
+    func test() {
+        print("popo")
+    }
+}
